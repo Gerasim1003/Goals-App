79b29a17453419f9a0ea26c59bed9212ed3972ff
Gerasim <gerasimm1003@gmail.com>
2019-09-05 14:55:55 +0400

    first commit

---
+++ Goals App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Goals App
+//
+//  Created by Gerasim Israyelyan on 9/5/19.
+//  Copyright © 2019 Gerasim Israyelyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
